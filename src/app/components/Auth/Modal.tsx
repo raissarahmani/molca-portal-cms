081d8b92a337eef70f6b4a8e011796e9d8e7b156
rrhmn <raissarahmani18@gmail.com>
2025-08-26 10:23:09 +0700

    add: auth page

---
+++ src/app/components/Auth/Modal.tsx
@@ -0,0 +1,13 @@
+import Login from './Login'
+// import Option from './Option'
+
+function Modal() {
+  return (
+    <div className='absolute flex flex-col inset-0 items-center justify-center gap-2'>
+      <Login />
+      {/* <Option /> */}
+    </div>
+  )
+}
+
+export default Modal
