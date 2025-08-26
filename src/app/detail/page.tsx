f69d080e711f96e60991605a428d84fe5bc5e269
rrhmn <raissarahmani18@gmail.com>
2025-08-26 17:40:06 +0700

    fix: layouting

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -3,38 +3,37 @@ import CardDetail from '@/app/components/Menu/Detail'
 export default function Detail() {
     const cards = [
     {
-      image: "/image.png",
+      image: "/digital-twin 1.png",
       title: "Free Roam",
       description: "Use your mouse to click and move to interact with objects "
     },
     {
-      image: "/image.png",
+      image: "/digital-twin 2.png",
       title: "Updated Information",
       description: "Access the updated site data and turn insights into action"
     },
     {
-      image: "/image.png",
+      image: "/digital-twin 3.png",
       title: "Chart and Documentation",
       description: "View historical site data and documentation such as equipment user manual"
     },
   ]
 
   return (
-    <div className='flex flex-col gap-3'>
-        <div className='absolute bg-[#231F20] rounded-md p-5 flex flex-col items-center justify-center gap-5 mx-10'>
-          <div className="flex flex-row flex-wrap gap-5 justify-between items-center">
-            {cards.map((card, i) => (
-              <CardDetail
-                key={i}
-                image={card.image}
-                title={card.title}
-                description={card.description}
-              />
-            ))}
-          </div>
+    <div className='absolute bg-transparent p-5 mx-30 flex flex-col items-center gap-3'>
+      <div className='bg-[#231F20] rounded-md p-5 flex flex-col items-center gap-3'>
+        <div className="flex flex-row justify-between items-center">
+          {cards.map((card, i) => (
+            <CardDetail
+              key={i}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
         </div>
-        <button type='button' className='bg-[var(--color-primary)] text-[var(--color-base)] button'>Start demo</button>
-    </div>
-    
+      </div>
+      <button type='button' className='bg-[var(--color-primary)] text-[var(--color-base)] button w-1/4'>Start demo</button>
+    </div>  
   )
 }
