2c1a918d77afcad204048fe16513b2ae43254862
rrhmn <raissarahmani18@gmail.com>
2025-08-26 15:42:41 +0700

    fix: global page route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Main from './pages/Main'
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import Loader from '@/app/components/Loader'
 
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <Main />
-    </main>
-  );
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const isLoggedIn = false // replace with real token check later
+      
+      if (isLoggedIn) {
+        router.push("/menu")
+      } else {
+        router.push("/auth")
+      }
+      
+      setLoading(false);
+    }, 1500)
+
+    return () => clearTimeout(timer)
+  }, [router])
+
+  if (loading) {
+    return <Loader />
+  }
+
+  return null
 }
